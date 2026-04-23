67b97d646c798cebe7dc73e72452a3aaab72b13b
abubekernuru <abubekernuru27@gmail.com>
2026-04-23 22:52:37 +0300

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
+import { Button } from 'flowbite-react'
 
 function App() {
   return (
-  <h1 className='text-taupe-700'>Hello, Vite!</h1>
+    <>   
+      <h1 className='text-taupe-700'>Hello, Vite!</h1>
+      <Button color="green">Green</Button>
+    </>
   )
 }
 
